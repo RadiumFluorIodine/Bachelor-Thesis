4a40a3ea79685303f031130d17c56e0275d9c3ee
RadiumFluorIodine_88953 <144572847+RadiumFluorIodine@users.noreply.github.com>
2025-12-27 19:16:18 +0700

    Update run_ablation.py

--- run_ablation.py
+++ run_ablation.py
@@ -1,1 +1,7 @@
+import torch, pandas as pd, numpy as np
+from src.models.utae import UTAE
+from src.models.baselines import ReUse
+from src.data.dataset import BiomassDataset
+from torch.utils.data import DataLoader, random_split
+
 
